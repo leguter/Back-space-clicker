c5bcb125837dcb799c333fe983dcb31731feace7
legut <levcenkokola06@gmail.com>
2025-10-15 19:16:46 +0300

    db

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 // /db/index.js
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // якщо ти на Render, це обов’язково!
 });
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+module.exports = pool;
