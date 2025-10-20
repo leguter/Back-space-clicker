724e6283d0b5c6ef93cb0a7cfcae344b55860812
legut <levcenkokola06@gmail.com>
2025-10-20 14:49:54 +0300

    5

--- routes/wheel.js
+++ routes/wheel.js
@@ -12,7 +12,7 @@ router.use(authMiddleware);
 router.post("/create_invoice", async (req, res) => {
   try {
     const { telegramId } = req.user;
-    const spinPrice = 10; // ❗️ 10 реальних Telegram Stars (XTR)
+    const spinPrice = 1; // ❗️ 10 реальних Telegram Stars (XTR)
 
     const botToken = process.env.BOT_TOKEN;
 
